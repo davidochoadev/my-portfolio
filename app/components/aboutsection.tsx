4837b0e1f22209d705948925c6766fa0658670f3
rominafabi <rominafabibusiness@gmail.com>
2022-12-16 19:12:10 +0100

    Feat: created progress bar for about section

--- app/components/aboutsection.tsx
+++ app/components/aboutsection.tsx
@@ -4,6 +4,7 @@ import { Progress } from "@material-tailwind/react";
 
 export default function AboutSection(data: any) {
   const setInView = data.setInView;
+  const products : any = data.products;
   return (
     <section className="relative h-screen flex justify-center snap-start">
       <InView
@@ -16,7 +17,14 @@ export default function AboutSection(data: any) {
         <Typography variant="h3" className="text-[#111827] self-center pt-4 text-center">
           My Languages from GitHub!
         </Typography>
-        <Progress value={50} color="blue" />
+        {
+        products.map((product : any) => (
+        <div key={product.id} className="mx-4 my-5">
+          <Typography variant="h6" className="font-firacodeBold text-blue-gray-600">{product.id}</Typography>
+          <Progress value={product.value} color={product.id === "TypeScript" && "blue" || product.id === "JavaScript" && "yellow" || product.id === "HTML" && "orange" || product.id === "CSS" && "light-blue" || product.id === "Others" && "purple"} className="my-2"/>
+        </div>
+        ))
+        }
       </div>
     </section>
   );
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,6 +5,18 @@ import HomeSection from "~/components/homesection";
 import WorksSection from "~/components/workssection";
 import MyNavBar from "./navbar";
 import { Theme, useTheme } from "../utils/theme-provider";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+  return json([
+    { id: "TypeScript", value: 45.45 },
+    { id: "JavaScript", value: 55.45 },
+    { id: "HTML" , value: 33.24 },
+    { id: "CSS" , value: 80.32 },
+    { id: "Others", value: 10.43 },
+  ]);
+};
 
 export default function Index() {
 /*   const user = useOptionalUser(); */
@@ -15,6 +27,8 @@ export default function Index() {
   const [contactsInView, contactsSetInView] = useState(false);
   const [homeInView, homeSetInView] = useState(false);
   const [theme, setTheme] = useTheme();
+  const products = useLoaderData<typeof loader>();
+  
 
   const toggleTheme = () => {
     setTheme(theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
@@ -29,7 +43,7 @@ export default function Index() {
     <main className="snap-y snap-mandatory h-screen overflow-scroll">
       <MyNavBar theme={theme} toggleTheme={toggleTheme} titleBox={titleBox} setTitleBox={setTitleBox} lang={lang} setLang={setLang} isView={inView} worksIsView={worksInView} contactsIsView={contactsInView} homeIsView={homeInView}/>
       <HomeSection titleBox={titleBox} lang={lang} homeSetInView={homeSetInView} />
-      <AboutSection setInView={setInView} />
+      <AboutSection products={products} setInView={setInView} />
       <WorksSection worksSetInView={worksSetInView} />
       <ContactsSection contactsSetInView={contactsSetInView} />
     </main>
