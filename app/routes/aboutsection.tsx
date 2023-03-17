4f0c4e5bb55ebc852b35f587fbdbc4735c734109
davideochoaa <davideochoaa@gmail.com>
2023-03-17 19:35:55 +0100

    Feat: testing push

--- app/routes/aboutsection.tsx
+++ app/routes/aboutsection.tsx
@@ -13,7 +13,7 @@ export default function AboutSection(data: any) {
       />
       <div className="mx-4 bg-white w-full h-full rounded-3xl self-center flex justify-center flex-col relative">
         <Typography variant="h3" className="text-[#111827] self-center pt-4 text-center">
-          My Languages from GitHub!
+          My Languages from GitHub!!
         </Typography>
         <iframe className="hidden md:inline-block" width="100%" height="100%" src="https://ionicabizau.github.io/github-profile-languages/api.html?davideochoaa" frameBorder="0" title="my programming languages stats"></iframe>
       </div>
