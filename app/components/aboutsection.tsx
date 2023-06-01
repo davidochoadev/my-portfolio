077de29f0c6b296eb2e9250d77fdb180b6e66dc1
davideochoaa <davideochoaa@gmail.com>
2023-06-01 23:54:53 +0200

    Fix: fix on aboutsection

--- app/components/aboutsection.tsx
+++ app/components/aboutsection.tsx
@@ -45,7 +45,7 @@ export default function AboutSection(data: any) {
         <MyCard lang={data.lang} dialogElement={1} products={products} preview={false} title={"My Coding Stats 🧑🏻‍💻"} italianTitle={"Le mie Statistiche 🧑🏻‍💻"} engContenuto={"These are my coding languages, WakaTime & GitHub Stats"} itaContenuto={"Queste sono le statistiche dei miei linguaggio di programmazione, di WakaTime e GitHub."} position={-100}>
           <div id="container-stats-1" className="w-full group-hover:hidden flex flex-col items-center">
             <a href="https://github.com/davideochoadev" className="my-2">
-              <img alt="David Stats" src="https://github-readme-stats.vercel.app/api/top-langs/?username=davideochoadev&layout=compact&hide_border=true&title_color=33DDB3" />
+              <img alt="David Stats" src="https://github-readme-stats.vercel.app/api/top-langs/?username=davidochoadev&layout=compact&hide_border=true&title_color=33DDB3" />
             </a>
           </div>
         </MyCard>
